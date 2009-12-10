449637c45d6e75320b55b0c9e3d2fc208e183f1a
cemsonmez <cemosonmez@gmail.com>
2009-12-10 18:24:27 +0000

    Sunucu tarafına, kullanıcı isimlerini kaydeden metot eklendi. istemcide publish islemi yapıldığı zaman çağrılıyor.

--- webapp/src/org/red5/core/Application.java
+++ webapp/src/org/red5/core/Application.java
@@ -3,61 +3,54 @@ package org.red5.core;
 import java.awt.List;
 import java.util.ArrayList;
 import java.util.Iterator;
-
 import org.red5.server.adapter.ApplicationAdapter;
 import org.red5.server.api.IConnection;
 import org.red5.server.api.IScope;
 import org.red5.server.api.Red5;
-import org.red5.server.api.service.IServiceCapableConnection;
 import org.red5.server.api.so.ISharedObject;
 import org.red5.server.api.so.ISharedObjectListener;
-import org.red5.logging.Red5LoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.red5.server.service.ServiceInvoker;
-import org.red5.server.stream.ClientBroadcastStream;
 
 public class Application extends ApplicationAdapter{
-		//StreamManager streamManager= new StreamManager();
+		/*Arraylist to store the connected client's usernames's*/
+		ArrayList<String> usernameArraylist= new ArrayList<String>();
 		String message="";
-		private static final Logger log = LoggerFactory.getLogger(Application.class);
-		
+		IScope appScope;
 
 	public boolean appStart (IScope scope) {
-		
-		//System.out.println(scope);
-		log.debug("Videoconference application started");
 		System.out.println("Videoconference application started");
-		createSharedObject(scope, "chat", true);
-		
-		ISharedObject so = getSharedObject(scope, "chat");		
-		ISharedObjectListener listener = new MyCustomListener();
-		
-		so.addSharedObjectListener(listener);
-		so.setAttribute(message, "zz");
-		
-		System.out.println(so.getName());
-		System.out.println(so.getPath());
-		System.out.println(so.getClass());
-		
 		return true;
 	}
 	
 	public void appStop (IScope scope) {
-		log.info("Videoconference Application stopped");
 		System.out.println("Videoconference Application stopped");
 		
 	}
 	public boolean connect (IConnection conn, IScope scope, Object []params) {
-		log.debug("Connected to server application - Client : "+ conn.getRemoteAddress());
 		System.out.println("Connected to server application - Client : "+ conn.getRemoteAddress());
+		appScope= scope;
+		createSharedObject(appScope, "chat", true);
+		ISharedObject so = getSharedObject(appScope, "chat");
+		ISharedObjectListener listener = new MyCustomListener();
+		so.addSharedObjectListener(listener);
 		return true;
 	}
 	public void disconnect (IConnection conn, IScope scope) {
-		log.debug("Application disconnect",conn.getRemoteAddress());
 		System.out.println("Application disconnect"+conn.getRemoteAddress());
 		super.disconnect(conn, scope);
 	}
+	
+	/*sends the connected usernames to the client method*/
+	public void sendUsername(String param) {
+		usernameArraylist.add(param);
+    	ISharedObject so = getSharedObject(appScope, "chat");
+    	so.sendMessage("receiveUsername",usernameArraylist);
+    	Iterator<String> iterator= usernameArraylist.iterator();
+    	while( iterator. hasNext() ) {
+    		System.out.println("param : "+iterator.next());
+    	}
+    }
 	
 	public boolean roomStart(IScope scope) {
 		  if (!super.roomStart(scope))
@@ -84,24 +77,28 @@ public class Application extends ApplicationAdapter{
 		super.roomDisconnect(conn);
 	}
 	
-	public void showStreamNames(String param) {
-		System.out.println("server side method"+param);
-		log.info("info mesajı");
+	public String getStreamName(String stream) {
+		System.out.println("server side method "+stream);
+		//log.info("info mesajı");
 		IConnection conn= Red5.getConnectionLocal();
-		
 		log.debug("Stream for : " +conn.getScope().getContextPath());
-		ArrayList<String> stream = new ArrayList<String>();
-		stream= (ArrayList<String>)this.getBroadcastStreamNames(conn.getScope());
-		Iterator iter= stream.iterator();
-		while(iter.hasNext()) {
+		ArrayList<String> streamList = new ArrayList<String>();
+		streamList= (ArrayList<String>)this.getBroadcastStreamNames(conn.getScope());
+		Iterator iterator= streamList.iterator();
+		while(iterator.hasNext()) {
 			try {
-				System.out.println(iter.next());
+				String streamName= iterator.next().toString();
+				//System.out.println(streamName);
+				if (streamName!=stream) {
+					System.out.println(streamName);
+					return streamName;
+				}
 			}
 			catch (Exception e) {
 				// TODO: handle exception
 				log.error("error with the stream",e);
 			}
 		}
-		
+		return "";
 	}
 }
