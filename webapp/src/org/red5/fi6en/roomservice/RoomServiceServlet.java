366b2dd0f51fd088f3a0f9114ac4673c3cc68a4a
kozdincer <kaanozdincer@gmail.com>
2010-10-04 08:43:21 +0000

    roomlist tamamlandı.

--- webapp/src/org/red5/fi6en/roomservice/RoomServiceServlet.java
+++ webapp/src/org/red5/fi6en/roomservice/RoomServiceServlet.java
@@ -2,13 +2,16 @@ package org.red5.fi6en.roomservice;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Random;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.red5.fi6en.core.Application;
 import org.red5.fi6en.util.HibernateUtil;
@@ -17,30 +20,44 @@ import org.slf4j.Logger;
 
 public class RoomServiceServlet extends HttpServlet {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	static SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 	private static Logger log = Red5LoggerFactory.getLogger(Application.class,"fi6en");
 	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		response.setContentType("text/html;charset=UTF-8");
-		
-		PrintWriter out = response.getWriter();
+response.setContentType("text/html;charset=UTF-8");
 		
 		StringBuilder sb = new StringBuilder();
-		sb.append("<users>");
-		Random r = new Random();
-		String[] ss = {"kozdincer","kaan","osman","sedat","orcun","mali","hakan","volkan"};
 		
+		//Select * from files
+		Session session = sessionFactory.openSession();
+		String sql = "select * from rooms where is_open = 1";
 		
-		for (int i=0; i<r.nextInt(20); i++) {
-			int a = r.nextInt(7);
-			sb.append("<user>");
-			sb.append("<id>" + "1" + "</id>");
-			sb.append("<username>" + ss[a] + "</username>");
-			sb.append("</user>");
+		SQLQuery query = session.createSQLQuery(sql);
+        query.addEntity("rooms", Room.class);
+        List results = query.list();
+        Iterator iter = results.iterator();
+		
+		sb.append("<rooms>");
+		while (iter.hasNext()) {
+			Room u = (Room) iter.next();
+			
+			sb.append("<room>");
+			
+			sb.append("<id>" + u.getId() + "</id>");
+			sb.append("<name>" + u.getName() + "</name>");
+			sb.append("<ispublic>" + u.isIs_public().toString() + "</ispublic>");
+			sb.append("<comment>" + u.getComment() + "</comment>");
+			
+			sb.append("</room>");
 		}
-		sb.append("</users>");
-		
+		sb.append("</rooms>");
+
+		PrintWriter out = response.getWriter();
 		out.print(sb.toString());
 
 		
